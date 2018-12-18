154bfd9f4843789dc6e8e37bfeaca9639dcfe819
mithilesht <mithilesht@cdac.in>
2018-12-18 11:51:24 +0530

    Project initialized

---
+++ src/main/java/in/cdac/hcdc/sanskritdictionary/SanskritDictionaryApplication.java
@@ -0,0 +1,14 @@
+package in.cdac.hcdc.sanskritdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanskritDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanskritDictionaryApplication.class, args);
+	}
+
+}
+
